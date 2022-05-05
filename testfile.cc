b7b8b20847539c7664801637ff7d59e2b29d5e87
Alan <69608277+alan1996colo@users.noreply.github.com>
2022-05-05 15:56:00 -0300

    Update testfile.cc
    Agregue algunos comenarios para entender mejor el codigo.
    Dejo referencia a la idea de como simular el switch.
    https://www.nsnam.org/doxygen/openflow-switch_8cc_source.html

--- testfile.cc
+++ testfile.cc
@@ -36,12 +36,12 @@ main (int argc, char *argv[])
   LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  NodeContainer nodes;
-  nodes.Create (2);
+  NodeContainer nodes;//Genera un contenedor de nodos nombre "nodes"
+  nodes.Create (2);//asigna 2 nodos a dicho contenedor
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  PointToPointHelper pointToPoint;//point to point es un OBJETO de tipo pointToPointHelper
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));//velocidad
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));//comienza en
 
   NetDeviceContainer devices;
   devices = pointToPoint.Install (nodes);
@@ -50,26 +50,26 @@ main (int argc, char *argv[])
   stack.Install (nodes);
 
   Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
+  address.SetBase ("10.1.1.0", "255.255.255.0");//esta es la direccion de toda la red.
 
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);//
 
-  UdpEchoServerHelper echoServer (9);
+  UdpEchoServerHelper echoServer (9);//asigna el puerto 9 al server
 
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-  serverApps.Start (Seconds (1.0));
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));//instala al nodo 2 (1) atributos del server
+  serverApps.Start (Seconds (1.0));//cuando arranca y cuando para el server
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);//hace la interfaz del cliente con el servidor 
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));//nodo 1 (0) es el cliente
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
   
-  NS_LOG_UNCOND ("Comienza del programa de prueba.");
+  NS_LOG_UNCOND ("Comienza del programa de prueba.");//esto sirve como un printf("")
   Simulator::Run ();
   NS_LOG_UNCOND ("Fin del programa de prueba.");
   Simulator::Destroy ();
