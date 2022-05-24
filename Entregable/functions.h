4b45f92793c5c74d285989f3976ddc2fb81a8362
Juan Manuel Corbera <jmcorbera@gmail.com>
2022-05-23 23:53:31 -0300

    update functions

--- Entregable/functions.h
+++ Entregable/functions.h
@@ -17,9 +17,11 @@ using namespace ns3;
 
 static Ptr<OutputStreamWrapper> cWndStream_n2i0;
 static Ptr<OutputStreamWrapper> ssThreshStream_n2i0;
+static bool first_SsThresh_n2i0 = true;
 
 static Ptr<OutputStreamWrapper> cWndStream_n3i0;
 static Ptr<OutputStreamWrapper> ssThreshStream_n3i0;
+static bool first_SsThresh_n3i0 = true;
 
 static void
 Cwnd_n2i0_Change (uint32_t oldval, uint32_t newval)
@@ -30,6 +32,11 @@ Cwnd_n2i0_Change (uint32_t oldval, uint32_t newval)
 static void
 SsThresh_n2i0_Change (uint32_t oldval, uint32_t newval)
 {
+  if (first_SsThresh_n2i0)
+  {
+    first_SsThresh_n2i0 = false;
+    return;
+  }
   *ssThreshStream_n2i0->GetStream () << Simulator::Now ().GetSeconds () << " " << newval << std::endl;
 }
 
@@ -42,6 +49,11 @@ Cwnd_n3i0_Change (uint32_t oldval, uint32_t newval)
 static void
 SsThresh_n3i0_Change (uint32_t oldval, uint32_t newval)
 {
+  if (first_SsThresh_n3i0)
+  {
+    first_SsThresh_n3i0 = false;
+    return;
+  }
   *ssThreshStream_n3i0->GetStream () << Simulator::Now ().GetSeconds () << " " << newval << std::endl;
 }
 
